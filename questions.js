236b05a25ef0e8348983476dfef1ec090417a88c
Nikki Newsom <nlnewsom@gmail.com>
2022-03-09 21:40:18 -0600

    Added the questions.

---
+++ questions.js
@@ -0,0 +1,93 @@
+export default [
+    {
+        type: 'list',
+        name: 'q1',
+        message: 'Crows are black birds known for their what?',
+        choices: [
+            "larger beaks, red wings, and soft chirps.",
+            "intelligence, adaptability and loud, harsh caw.",
+            "friendliness to humans, love for peanuts, and quack.",
+            "hooked beaks, wide wingspan, and high-pitched whistling.",
+
+        ]
+    },
+
+    {
+        type: 'list',
+        name: 'q2',
+        message: 'What family do Crows belong to?',
+        choices: [
+            "Corvidae",
+            "Anatidae",
+            "Cracidae",
+            "Columbidae",
+
+        ]
+    },
+
+    {
+        type: 'list',
+        name: 'q3',
+        message: 'What have crows been observed making and using?',
+        choices: [
+            "eating utensils",
+            "beds",
+            "basic tools",
+            "traps",
+
+        ]
+    },
+
+    {
+        type: 'list',
+        name: 'q4',
+        message: 'Of all the living birds, crows and ravens have the biggest brain-to-body size ratio. They have been said to be as intelligent as:',
+        choices: [
+            "Apes and Dolphins",
+            "Dogs and Cats",
+            "Alligators and Crocodiles",
+            "Squirrels and Rabbits",
+
+        ]
+    },
+
+    {
+        type: 'list',
+        name: 'q5',
+        message: 'Crows can recognize your face for up to half a decade and even hold a grudge.',
+        choices: [
+            "larger beaks, red wings, and soft chirps.",
+            "intelligence, adaptability and loud, harsh caw.",
+            "friendliness to humans, love for peanuts, and quack.",
+            "hooked beaks, wide wingspan, and high-pitched whistling.",
+
+        ]
+    },
+
+    {
+        type: 'list',
+        name: 'q6',
+        message: 'Crows mostly mate for life and band together in tight-knit families.',
+        choices: [
+            "larger beaks, red wings, and soft chirps.",
+            "intelligence, adaptability and loud, harsh caw.",
+            "friendliness to humans, love for peanuts, and quack.",
+            "hooked beaks, wide wingspan, and high-pitched whistling.",
+
+        ]
+    },
+
+    {
+        type: 'list',
+        name: 'q7',
+        message: 'A group of crows is called a:',
+        choices: [
+            "Conspiracy",
+            "Pool",
+            "Murder",
+            "Team",
+
+        ]
+    },
+
+]
